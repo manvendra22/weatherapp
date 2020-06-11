a25b605066f2942bc99d9ad89c58af7668650813
Manvendra Singh <mjdeora@gmail.com>
2020-06-11 21:08:58 +0530

    [Binding] Area chart

--- src/WeatherCard/AreaChart.js
+++ src/WeatherCard/AreaChart.js
@@ -1,83 +1,10 @@
 /* eslint-disable react/no-multi-comp */
 import React from 'react';
+import moment from 'moment';
+
 import {
     AreaChart, Area, XAxis, YAxis, ResponsiveContainer
 } from 'recharts';
-
-const data = [
-    {
-        time: '9am', temp: 21,
-    },
-    {
-        time: '10am', temp: 22,
-    },
-    {
-        time: '11am', temp: 24,
-    },
-    {
-        time: '12am', temp: 26,
-    },
-    {
-        time: '1pm', temp: 29,
-    },
-    {
-        time: '2pm', temp: 29,
-    },
-    {
-        time: '3pm', temp: 29,
-    },
-    {
-        time: '4pm', temp: 28,
-    },
-    {
-        time: '5pm', temp: 27,
-    },
-    {
-        time: '6pm', temp: 26,
-    },
-    {
-        time: '7pm', temp: 25,
-    },
-    {
-        time: '8pm', temp: 24,
-    },
-    {
-        time: '9pm', temp: 24,
-    },
-    {
-        time: '10pm', temp: 23,
-    },
-    {
-        time: '11pm', temp: 22,
-    },
-    {
-        time: '12am', temp: 20,
-    },
-    {
-        time: '1am', temp: 19,
-    },
-    {
-        time: '2am', temp: 17,
-    },
-    {
-        time: '3am', temp: 17,
-    },
-    {
-        time: '4am', temp: 16,
-    },
-    {
-        time: '5am', temp: 16,
-    },
-    {
-        time: '6am', temp: 17,
-    },
-    {
-        time: '7am', temp: 20,
-    },
-    {
-        time: '8am', temp: 20,
-    },
-];
 
 function CustomizedAxisTick(props) {
     const {
@@ -91,7 +18,7 @@ function CustomizedAxisTick(props) {
     );
 }
 
-const gradientOffset = () => {
+const gradientOffset = (data) => {
     const dataMax = Math.max(...data.map(i => i.temp));
     const dataMin = Math.min(...data.map(i => i.temp));
 
@@ -108,14 +35,27 @@ const gradientOffset = () => {
     return dataMax / (dataMax - dataMin);
 };
 
-const off = gradientOffset();
-console.log({ off })
 
-export default function MainChart() {
+export default function MainChart(props) {
+    const { data = [], selected } = props
+
+    const chartData = data.map(value => {
+        return {
+            time: moment.unix(value.dt).format('ha'), temp: Math.round(value.temp),
+        }
+    })
+
+    const firstChartData = chartData.filter((value, index) => index < 24)
+    const secondChartData = chartData.filter((value, index) => index > 23)
+
+    const mainChartData = selected % 2 === 0 ? firstChartData : secondChartData
+
+    const off = gradientOffset(mainChartData);
+
     return (
         <ResponsiveContainer width='100%' height={150}>
             <AreaChart
-                data={data}
+                data={mainChartData}
             >
                 <XAxis dataKey="time" tickLine={false} height={50} interval={0} tick={false} />
                 <YAxis domain={[20, 'auto']} hide={true} />
