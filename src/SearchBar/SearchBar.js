22c710b01fd3a36f004f65c4b34809d9c8aa42b3
Manvendra Singh <mjdeora@gmail.com>
2020-06-12 03:42:22 +0530

    [Fix] Search bar

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -17,14 +17,16 @@ function SearchBar(props) {
                 <img src={search} alt="dayIcon" className="inputIcon" />
             </div>
             {Object.keys(cityData).length && cityData.cod === 200 ?
-                <div className="cityData" onClick={handleCityClick}>
-                    <div className="grayText">{`${cityData.name}, ${cityData.sys.country}`}</div>
-                    <div className="cityWeather">
-                        <div className="mr-10">
-                            <div className="boldText">{Math.round(cityData.main.temp)}&deg; C</div>
-                            <div className="smallText grayText">Rain</div>
+                <div className="cityContainer">
+                    <div className="cityData" onClick={handleCityClick}>
+                        <div className="grayText">{`${cityData.name}, ${cityData.sys.country}`}</div>
+                        <div className="cityWeather">
+                            <div className="mr-10">
+                                <div className="boldText">{Math.round(cityData.main.temp)}&deg; C</div>
+                                <div className="smallText grayText">Rain</div>
+                            </div>
+                            <img src={getIcon(cityData.weather[0].id)} alt="dayIcon" className="smallIcon" />
                         </div>
-                        <img src={getIcon(cityData.weather[0].id)} alt="dayIcon" className="smallIcon" />
                     </div>
                 </div> : null}
         </div>
