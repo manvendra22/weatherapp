8feb32c5826302055548b058823cc24d94249cee
Manvendra Singh <mjdeora@gmail.com>
2020-06-12 12:17:30 +0530

    [Feature] More icons

--- src/utility.js
+++ src/utility.js
@@ -1,13 +1,15 @@
 import sun from './icons/sun.svg'
 import rain from './icons/rain.svg'
 import cloudy from './icons/cloudy.svg'
+import drizzle from './icons/drizzle.svg'
+import thunderstorm from './icons/thunderstorm.svg'
 
 export function getIcon(id) {
     // '800': sun,
     // '50x': rain,
     // '80x': cloudy,
     // '30x': 'drizzle',
-    // '20x': 'thunder'
+    // '20x': 'thunderstorm'
 
     switch (id) {
         case 800:
@@ -17,6 +19,10 @@ export function getIcon(id) {
         case 803:
         case 804:
             return cloudy;
+        case String(id).includes('30'):
+            return drizzle;
+        case String(id).includes('20'):
+            return thunderstorm;
         default:
             return rain;
     }
