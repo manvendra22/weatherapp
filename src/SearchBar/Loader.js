066eaed2d92091c1eeca9364da80104edead9bd8
Manvendra Singh <mjdeora@gmail.com>
2020-06-12 18:41:10 +0530

    [Fix] Binding bug

--- src/SearchBar/Loader.js
+++ src/SearchBar/Loader.js
@@ -4,10 +4,10 @@ import ContentLoader from "react-content-loader"
 const Loader = () => (
     <ContentLoader
         width={'100%'}
-        height={50}
+        height={55}
     >
-        <rect x="5%" y="10" rx="5" ry="5" width="90%" height="10" />
-        <rect x="5%" y="30" rx="5" ry="5" width="90%" height="10" />
+        <rect x="5%" y="12" rx="5" ry="5" width="90%" height="10" />
+        <rect x="5%" y="32" rx="5" ry="5" width="90%" height="10" />
     </ContentLoader>
 )
 
