111e9e521dd3c2bc70bdb657ff585e2378f58974
Manvendra Singh <mjdeora@gmail.com>
2020-06-12 11:01:21 +0530

    [Feature] Fonts updated

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './fonts/HomepageBaukasten-Book.ttf';
+import './fonts/HomepageBaukasten-Bold.ttf';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
