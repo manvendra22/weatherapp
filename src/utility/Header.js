4f786f1b1385cb75fa108d33d450a06129e6673c
Manvendra Singh <mjdeora@gmail.com>
2020-06-18 19:31:44 +0530

    [Feature] Github link

--- src/utility/Header.js
+++ src/utility/Header.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import moon from '../icons/moon.svg'
 import sun from '../icons/sun_dot.svg'
+import gitLogo from '../icons/github.svg'
 
 export default function Header({ toggleTheme, currentMode }) {
     return (
-        <div className="toggleIcon" onClick={toggleTheme}>
-            <img src={currentMode === 'light' ? moon : sun} className="bigIcon" alt="" />
-        </div>
+        <>
+            <div className="toggleIcon">
+                <img src={currentMode === 'light' ? moon : sun} className="toggleIcon" alt="toggle-logo" onClick={toggleTheme} />
+            </div>
+            <a target="_blank" href="https://github.com/manvendra22/weathercheck" className="gitLink">
+                <img src={gitLogo} alt="github-logo" className="gitIcon" />
+            </a>
+        </>
     );
 }
