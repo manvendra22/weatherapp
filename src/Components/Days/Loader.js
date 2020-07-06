e9a65bc410c53f6ed97f91d7c5977434aa76ae6f
Manvendra Singh <mjdeora@gmail.com>
2020-07-06 17:46:56 +0530

    [Style] Loader updated

--- src/Components/Days/Loader.js
+++ src/Components/Days/Loader.js
@@ -4,17 +4,18 @@ import ContentLoader from "react-content-loader"
 const Loader = () => (
     <ContentLoader
         width={'100%'}
-        height={110}
+        height={102}
         backgroundColor="var(--color-backgroundColor)"
         foregroundColor="var(--color-foregroundColor)"
     >
-        <rect x="20" y="0" rx="5" ry="5" width="70" height="110" />
-        <rect x="120" y="0" rx="5" ry="5" width="70" height="110" />
-        <rect x="220" y="0" rx="5" ry="5" width="70" height="110" />
-        <rect x="320" y="0" rx="5" ry="5" width="70" height="110" />
-        <rect x="420" y="0" rx="5" ry="5" width="70" height="110" />
-        <rect x="520" y="0" rx="5" ry="5" width="70" height="110" />
-        <rect x="620" y="0" rx="5" ry="5" width="70" height="110" />
+        <rect x="20" y="0" rx="5" ry="5" width="72" height="102" />
+        <rect x="110" y="0" rx="5" ry="5" width="72" height="102" />
+        <rect x="200" y="0" rx="5" ry="5" width="72" height="102" />
+        <rect x="290" y="0" rx="5" ry="5" width="72" height="102" />
+        <rect x="380" y="0" rx="5" ry="5" width="72" height="102" />
+        <rect x="470" y="0" rx="5" ry="5" width="72" height="102" />
+        <rect x="560" y="0" rx="5" ry="5" width="72" height="102" />
+        <rect x="650" y="0" rx="5" ry="5" width="72" height="102" />
     </ContentLoader>
 )
 
--- src/Components/WeatherCard/Loader.js
+++ src/Components/WeatherCard/Loader.js
@@ -4,16 +4,16 @@ import ContentLoader from "react-content-loader"
 const Loader = () => (
     <ContentLoader
         width={'100%'}
-        height={500}
+        height={640}
         backgroundColor="var(--color-backgroundColor)"
         foregroundColor="var(--color-foregroundColor)"
     >
-        <rect x="0" y="20" rx="5" ry="5" width="150" height="10" />
-        <rect x="0" y="40" rx="5" ry="5" width="150" height="10" />
-        <rect x="0" y="60" rx="5" ry="5" width="150" height="10" />
-        <rect x="0" y="100" rx="5" ry="5" width="100%" height="120" />
-        <rect x="0" y="240" rx="5" ry="5" width="100%" height="50" />
-        <rect x="0" y="310" rx="5" ry="5" width="100%" height="160" />
+        <rect x="0" y="10" rx="5" ry="5" width="175" height="10" />
+        <rect x="0" y="30" rx="5" ry="5" width="175" height="10" />
+        <rect x="0" y="50" rx="5" ry="5" width="175" height="10" />
+        <rect x="0" y="90" rx="5" ry="5" width="100%" height="180" />
+        <rect x="0" y="310" rx="5" ry="5" width="100%" height="60" />
+        <rect x="0" y="410" rx="5" ry="5" width="100%" height="210" />
     </ContentLoader>
 )
 
