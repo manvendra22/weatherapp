71d2ecf7be1785b2f64bec1fbf091d6b132b4d11
Manvendra Singh <mjdeora@gmail.com>
2020-09-03 00:37:29 +0530

    [Fix] Dark mode bug

--- src/utility/theme.js
+++ src/utility/theme.js
@@ -1,42 +1,27 @@
 import './theme.css'
 
-const COLORS = {
-    '--color-white': '#FFF',
-    '--color-black': '#000',
-    '--color-gray': '#BDBDBD',
-    '--color-granite-gray': '#666667',
-    '--color-picton-blue': '#3AABEA',
-    '--color-milk': '#FFFDF5',
-    '--color-alice-blue': '#F5F6F7',
-    '--color-whisper': '#EEE',
-    '--color-eerie-black': '#161625',
-    '--color-dark-gunmetal': '#1E1E30',
-    '--color-aurometalsaurus': '#6C757D',
-    '--color-black-rock': '#2D2D3B',
-    '--color-gun-powder': '#454551',
-    '--color-scarpa-flow': '#585862'
-}
-
 export const LIGHT = {
-    '--color-bg': COLORS['--color-white'],
-    '--color-text-primary': COLORS['--color-black'],
-    '--color-text-secondary': COLORS['--color-granite-gray'],
-    '--color-primary': COLORS['--color-picton-blue'],
-    '--color-divider': COLORS['--color-gray'],
-    '--color-highlight': COLORS['--color-milk'],
-    '--color-highlight-bg': COLORS['--color-white'],
-    '--color-backgroundColor': COLORS['--color-alice-blue'],
-    '--color-foregroundColor': COLORS['--color-whisper']
+    '--color-bg': '#FFF',
+    '--color-text-primary': '#000',
+    '--color-text-secondary': '#666667',
+    '--color-primary': '#3AABEA',
+    '--color-divider': '#BDBDBD',
+    '--color-highlight': '#FFFDF5',
+    '--color-highlight-bg': '#FFF',
+    '--color-search-bg': '#FFF',
+    '--color-backgroundColor': '#F5F6F7',
+    '--color-foregroundColor': '#EEE',
 }
 
 export const DARK = {
-    '--color-bg': COLORS['--color-eerie-black'],
-    '--color-text-primary': COLORS['--color-gray'],
-    '--color-text-secondary': COLORS['--color-gray'],
-    '--color-primary': COLORS['--color-picton-blue'],
-    '--color-divider': COLORS['--color-gray'],
-    '--color-highlight': COLORS['--color-dark-gunmetal'],
-    '--color-highlight-bg': COLORS['--color-black-rock'],
-    '--color-backgroundColor': COLORS['--color-gun-powder'],
-    '--color-foregroundColor': COLORS['--color-scarpa-flow'],
+    '--color-bg': '#161625',
+    '--color-text-primary': '#BDBDBD',
+    '--color-text-secondary': '#BDBDBD',
+    '--color-primary': '#3AABEA',
+    '--color-divider': '#BDBDBD',
+    '--color-highlight': '#1E1E30',
+    '--color-highlight-bg': '#2D2D3B',
+    '--color-search-bg': '#212130',
+    '--color-backgroundColor': '#454551',
+    '--color-foregroundColor': '#585862'
 }
